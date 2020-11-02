0688a50fd1db09f443a3706100997049facef980
Taster98 <luigi.gesuele98@gmail.com>
2020-11-02 15:52:05 +0100

    Added if-then-else program

---
+++ Common/ifthenelse.s
@@ -0,0 +1,23 @@
+@ Semantica if then else: if(x==0) x++; else y++; con x -> R0, y -> R1
+
+@ Definisco dove sta il mio codice:
+
+.text
+.global main
+
+@ Scrivo il main
+
+main:	mov R1, #7	
+	mov R0, #1
+	@ mov R0, #0
+	cmp R0, #0
+	beq then @ se la condizione è vera vado al ramo then, sennò skippo questo e vado all'else
+	sub r1, r1, #1
+	b end
+then:	add r0, r0, #1
+end:	mov R0, R0
+
+@ syscall per la exit
+	mov r7, #1
+	svc 0
+	
