0fb9419191fb7af4c7bb97a2c25d75cc323619a8
Taster98 <luigi.gesuele98@gmail.com>
2021-01-19 14:58:20 +0100

    Added comments in A ex

--- 4Quiz/A/solution.s
+++ 4Quiz/A/solution.s
@@ -4,48 +4,45 @@
 .type cambia %function
 
 @in R0 ho c, e in R1 ho n
-cifra:	mov R2, R0 @Sposto c in R2 (unsigned int cc;)
-	cmp R0, #0x7a  @confronto c con 'z'
-	bgt skip
-	cmp R0, #0x61
-	blt skip
-	add R3,R0,R1 @Se arrivo qui vuol dire che sono nel range a-z, quindi in cc = c+n;
-	mov R2, R3
-	cmp R2, #0x7a @if(cc>'z') ..
-	BLE skip
-	sub R2, R2, #0x7a @In cc metto cc-'z'
-	add R2, R2, #0x61 @In cc aggiungo 'a'
-	sub R2, R2, #1 @In cc sottraggo 1
-skip:	mov R0, R2
+cifra:	mov R2, R0 @ Sposto c in R2 (unsigned int cc = c;)
+	cmp R0, #0x7a  @ Confronto c con 'z'
+	bgt skip	   @ Se è maggiore di 'z' skippo
+	cmp R0, #0x61  @ Confronto c con 'a'
+	blt skip	   @ Se è minore di 'a' skippo
+	add R2,R0,R1   @ Se arrivo qui vuol dire che sono nel range 'a'-'z', quindi faccio cc = c+n;
+	cmp R2, #0x7a  @ Confronto cc con 'z'
+	ble skip	   @ Se è minore o uguale skippo
+	sub R2, R2, #0x7a @ In cc metto cc-'z'
+	add R2, R2, #0x61 @ In cc aggiungo 'a'
+	sub R2, R2, #1    @ In cc sottraggo 1
+skip:	mov R0, R2    @ Il valore di ritorno è cc che sta in R2, lo sposto in R0 e posso fare la return
 	mov PC, LR
 
 @ Ora qui implemento la funzione cambia
 @ In R0 ho la mia stringa e in R1 ho la sua lunghezza
-cambia:	push {R0-R1, LR} @Salvo questi parametri nello stack perché chiamo strlen
+cambia:	push {R0-R1, LR} @ Salvo questi parametri nello stack perché chiamo strlen
 	bl strlen
-	mov R2, R0  @Salvo il valore di ritorno in R2
-	pop {R0-R1, LR}
-	@In R2 ho la lunghezza della stringa
-	mov R3, #0 @sum = 0
-	mov R4, #0 @i=0
-for:	cmp R4, R2 @confronto i con ls
-	bge fine
-	push {R0-R3, LR} @salvo i parametri della funzione cambia
-	@In R0 devo caricare il carattere corrente, con una LDRB
-	ldrb R6, [R0,R4] @Metto in R6 l'elemento i-esimo di R0 che ha il vettore
-	mov R0, R6
-	bl cifra
-	mov R5, R0 @In R5 ho il valore di ritorno di cifra
-	pop {R0-R3, LR}
-	cmp R5,R6
-	beq skip2
-	mov R6, R5
-	strb R6,[R0,R4] @memorizzo il cambiamento
-	add R3, R3, #1 @incremento sum
-skip2:	add R4, R4, #1
-	b for
-fine:	mov R0, R3
-	mov PC, LR
+	mov R2, R0  @ Salvo il valore di ritorno di strlen in R2 (ls = strlen(s))
+	pop {R0-R1, LR} @ Recupero i valori precedentemente salvati
+	mov R3, #0 @ sum = 0
+	mov R4, #0 @ i=0
+for:	cmp R4, R2 @ confronto i con ls (lungh stringa)
+	bge fine	   @ Se i >= ls allora ho finito di iterare
+	push {R0-R3, LR} @ Salvo i parametri della funzione cambia
+	ldrb R6, [R0,R4] @ Metto in R6 l'elemento i-esimo di R0 che ha il vettore, usando LDRB
+	mov R0, R6       @ Sposto il valore in R0 prima di chiamare la funzione cifra, perché deve essere il primo parametro
+	bl cifra		 @ Il secondo parametro non lo abbiamo spostato quindi possiamo chiamarla subito
+	mov R5, R0 		 @ In R5 ho il valore di ritorno di cifra (cc = cifra)
+	pop {R0-R3, LR}	 @ Recupero i valori precedentemente salvati
+	cmp R5,R6		 @ Confronto cc con s[i]
+	beq skip2		 @ Se sono uguali skippo
+	mov R6, R5		 @ Se differiscono allora aggiorno s[i] con STRB e incremento sum:
+	strb R6,[R0,R4]  @ Memorizzo il cambiamento
+	add R3, R3, #1 	 @ Incremento sum
+skip2:	add R4, R4, #1 @ Incremento l'indice i del for
+	b for			   @ Rivado a for
+fine:	mov R0, R3	   @ Se sono qui ho finito di iterare, e devo ritornare sum che è in R3, lo sposto in R0
+	mov PC, LR		   @ Ritorno
 	
 	
 
